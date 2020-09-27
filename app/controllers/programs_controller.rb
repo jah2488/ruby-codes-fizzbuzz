88db407a287f9481fa816e7b039641ee83e38278
mattrice12 <mattrice12@outlook.com>
2020-09-26 20:40:03 -0500

    Add React, convert Application views to React, add ApiClient

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,24 +1,45 @@
 class ProgramsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def show
-    program = Program.find(params.fetch(:id))
-    render locals: { program: program }
+    program = Program.includes(:chars).find(params.fetch(:id))
+    
+    if request.xhr?
+      render json: view(program)
+    else
+      render :show
+    end
   end
 
   def update
     program = Program.find(params.fetch(:id))
-    char = program.chars.find_or_create_by(name: params.dig(:program, :addition))
+    char = program.chars.find_or_create_by(name: params.fetch(:addition))
     Vote.create(char: char)
     if char.votes_count >= Program::VOTE_THRESHOLD[program.mode]
       program.update(code: "#{program.code} #{char.name}")
       program.chars.destroy_all
     end
-    redirect_to program
+
+    render json: view(program)
   end
 
   def clear
     program = Program.find(params.fetch(:id))
     program.update(code: "")
     program.chars.destroy_all
-    redirect_to program
+
+    render json: view(program)
+  end
+
+  private
+
+  def view(program)
+    {
+      id: program.id,
+      name: program.name,
+      mode: program.mode,
+      code: program.code,
+      chars: program.chars.order(votes_count: :desc)
+    }
   end
 end
---
+++ app/javascript/packs/App.jsx
@@ -0,0 +1,14 @@
+// Run this example by adding <%= javascript_pack_tag 'App_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>App React</div> at the bottom
+// of the page.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Program from "../components/Program"
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Program name="React" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
