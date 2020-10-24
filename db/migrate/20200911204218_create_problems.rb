b0fae6009b7b6af80eed948baf0daec0d0d5921f
Justin Herrick <justin@justinherrick.com>
2020-10-24 15:36:00 -0500

    Cleaning up migrations.

--- db/migrate/20200911204218_create_problems.rb
+++ db/migrate/20200911204218_create_problems.rb
@@ -3,7 +3,9 @@ class CreateProblems < ActiveRecord::Migration[6.0]
     create_table :programs do |t|
       t.string :name, null: false
 
-      t.string :mode, default: "Anarchy", null: false
+      t.text :code, default: "", null: false
+      t.text :boilerplate, default: "", null: false
+      t.string :mode, default: "anarchy", null: false
       t.boolean :complete, default: false, null: false
       t.integer :tick, default: 0, null: false
       t.jsonb :settings, default: {}, null: false
--- db/migrate/20200920191440_add_code_to_programs.rb
+++
@@ -1,5 +0,0 @@
-class AddCodeToPrograms < ActiveRecord::Migration[6.0]
-  def change
-    add_column :programs, :code, :text, null: false, default: ""
-  end
-end
