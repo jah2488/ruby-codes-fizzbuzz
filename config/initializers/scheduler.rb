792fe09ae853c25bc4a747a16841402ecb3ee007
Justin Herrick <justin@justinherrick.com>
2020-11-06 11:24:06 -0600

    Adding packages.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,8 +1,10 @@
 require 'rufus-scheduler'
 
-Rufus::Scheduler.singleton.every '1s' do
-  Program.running.each do |program|
-    program.update(tick: program.tick.succ) # Rails.logger.debug "Incrementing Game Tick for <Program##{program.id}> to [#{program.tick}]"
+if Rails.env.development?
+  Rufus::Scheduler.singleton.every '1s' do
+    Program.running.each do |program|
+      program.update(tick: program.tick.succ) # Rails.logger.debug "Incrementing Game Tick for <Program##{program.id}> to [#{program.tick}]"
+    end
+    Rails.logger.flush
   end
-  Rails.logger.flush
 end
