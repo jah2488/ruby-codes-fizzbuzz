46d33b1f0ed005b96b35346820810f529aa08cc8
Justin Herrick <justin@justinherrick.com>
2020-11-10 16:25:53 -0600

    Adding :sp special char and fixing backspace issue.

--- app/models/char.rb
+++ app/models/char.rb
@@ -1,18 +1,19 @@
 class Char < ApplicationRecord
   belongs_to :program
   has_many :votes, dependent: :destroy
-
   validates :name, presence: true
 
   COMMANDS = {
     TAB: ":t",
     NEW_LINE: ":nl",
-    BACKSPACE: ":bk"
+    BACKSPACE: ":bk",
+    SPACE: ":sp",
   }
 
   SPECIAL_CHAR = {
     COMMANDS[:TAB] => "\t",
-    COMMANDS[:NEW_LINE] => "\n"
+    COMMANDS[:NEW_LINE] => "\n",
+    COMMANDS[:SPACE] => " "
   }.freeze
 
   def formatted_name
