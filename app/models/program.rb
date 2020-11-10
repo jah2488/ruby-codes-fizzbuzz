707aee70518c5018331cbe2030b1415d29fc4732
mattrice12 <mattrice12@outlook.com>
2020-11-10 17:53:54 -0600

    updated backspace

--- app/models/program.rb
+++ app/models/program.rb
@@ -56,15 +56,14 @@ class Program < ApplicationRecord
   end
 
   def handle_backspace
-    delimiter = case settings["max_input_mode"]
-                when Program.max_input_modes["char"] then ""
-                when Program.max_input_modes["word"] then " "
-                when Program.max_input_modes["line"] then "\n"
-                else
-                  ""
-                end
+    case settings["max_input_mode"]
+    when Program.max_input_modes["char"] then code.split("", -1)[0..-3].join("")
+    when Program.max_input_modes["word"] then code.split(/ /, -1)[0..-2].join(" ")
+    when Program.max_input_modes["line"] then code.split("\n", -1)[0..-2].join("\n")
+    else
+      ""
+    end
     # split(//, -1) is necessary to prevent over-deletion when the last character is a new-line
-    code.split("#{delimiter}", -1)[0..-3].join("#{delimiter}")
   end
 
   def playing?
