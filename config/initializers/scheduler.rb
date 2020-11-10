3578ba8c8c7bc54e4dc72bc85eeee3d99a824639
Justin Herrick <justin@justinherrick.com>
2020-11-10 14:07:21 -0600

    Fixing init issue with scheduler.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,10 +1,13 @@
-require 'rufus-scheduler'
+begin
+  require 'rufus-scheduler'
 
-if Rails.env.development?
   Rufus::Scheduler.singleton.every '1s' do
     Program.running.each do |program|
       program.update(tick: program.tick.succ) # Rails.logger.debug "Incrementing Game Tick for <Program##{program.id}> to [#{program.tick}]"
     end
     Rails.logger.flush
   end
+rescue StandardError, LoadError => e
+  Rails.logger.error "Error in Scheduler: #{e.message}"
 end
+ 
