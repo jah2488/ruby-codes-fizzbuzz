98557652c730e2f7e6f5ef8159fd256278290086
Justin Herrick <justin@justinherrick.com>
2021-01-17 15:43:52 -0600

    Removing commented out code.

--- app/channels/admin_program_channel.rb
+++ app/channels/admin_program_channel.rb
@@ -111,44 +111,6 @@ class AdminProgramChannel < ApplicationCable::Channel
     puts "receive:#{data}"
   end
 
-  # def evaluate_code
-  #   ProgramChannel.broadcast_to(room, {
-  #     action: :output,
-  #     data: current_program.evaluate
-  #   })
-  # end
-
-  # def message(data)
-  #   program = current_program
-  #   addition = data.fetch("addition")
-  #   is_code = data.fetch("isCode")
-
-  #   program.messages.create(name: addition, is_code: is_code, user: current_user)
-
-  #   if is_code
-  #     char = program.chars.find_or_create_by(name: addition)
-  #     if program.anarchy?
-  #       program.with_lock do
-  #         program.process_addition(addition)
-  #       end
-  #     else
-  #       Vote.create(char: char)
-  #       if char.votes_count >= program.settings["vote_threshold"]
-  #         program.with_lock do
-  #           program.process_addition(addition)
-  #         end
-  #         program.chars.destroy_all
-  #       end
-  #     end
-  #   end
-
-  #   ProgramChannel.broadcast_to(room, {
-  #     action: :message,
-  #     data: program.view
-  #   })
-  #   evaluate_code
-  # end
-
   def clear
     program = current_program
     program.entries.destroy_all
